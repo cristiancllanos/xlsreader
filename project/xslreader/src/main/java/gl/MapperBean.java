3240b2397c4885c77c9967130649f4ab9d0e7372
cristiancllanos <cristiancllanos@hotmail.com>
2012-09-14 20:17:43 -0300

    'changes'

--- project/xslreader/src/main/java/gl/MapperBean.java
+++ project/xslreader/src/main/java/gl/MapperBean.java
@@ -5,6 +5,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
+import com.google.gdata.client.spreadsheet.FeedURLFactory;
+import com.google.gdata.client.spreadsheet.SpreadsheetQuery;
 import com.google.gdata.client.spreadsheet.SpreadsheetService;
 import com.google.gdata.data.spreadsheet.SpreadsheetEntry;
 import com.google.gdata.data.spreadsheet.SpreadsheetFeed;
@@ -14,8 +16,11 @@ import com.google.gdata.util.ServiceException;
 public class MapperBean {
 
 	public List<Client> mapper(SourceMappingXLSFile sp)
-	throws AuthenticationException, MalformedURLException, IOException, ServiceException{
+			throws AuthenticationException, MalformedURLException, IOException,
+			ServiceException {
 
+		bla();
+		
 		SpreadsheetService service = new SpreadsheetService(
 				"MySpreadsheetIntegration-v1");
 
@@ -23,10 +28,9 @@ public class MapperBean {
 		// sections)
 
 		// Define the URL to request. This should never change.
-//		URL SPREADSHEET_FEED_URL = new URL(
-//				"https://spreadsheets.google.com/feeds/spreadsheets/private/full");
+		// URL SPREADSHEET_FEED_URL = new URL(
+		// "https://spreadsheets.google.com/feeds/spreadsheets/private/full");
 		URL SPREADSHEET_FEED_URL = new URL(sp.getXlsUrlSource());
-		
 
 		// Make a request to the API and get all spreadsheets.
 		SpreadsheetFeed feed = service.getFeed(SPREADSHEET_FEED_URL,
@@ -40,6 +44,25 @@ public class MapperBean {
 		}
 
 		return null;
+	}
+
+	public void bla() throws IOException, ServiceException {
+		String username = "cristian.llanos@gmail.com";
+		String password = "";
+		String spreadsheet = "Clientes";
+		SpreadsheetService service = new SpreadsheetService(
+				"MySpreadsheetIntegration-v1");
+		service.setUserCredentials(username, password);
+		FeedURLFactory factory = FeedURLFactory.getDefault();
+		SpreadsheetQuery spreadsheetQuery = new SpreadsheetQuery(
+				factory.getSpreadsheetsFeedUrl());
+		spreadsheetQuery.setTitleQuery(spreadsheet);
+		SpreadsheetFeed spreadsheetFeed = service.query(spreadsheetQuery,
+				SpreadsheetFeed.class);
+		List<SpreadsheetEntry> spreadsheets = spreadsheetFeed.getEntries();
+		
+		System.out.println(spreadsheets.size());
+		
 	}
 
 }
